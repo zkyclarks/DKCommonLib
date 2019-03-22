39b39c97f4f2b9e2957c5c4c2bd67dbddc120e59
clark <clark@clarkdeiMac.local>
2019-03-22 15:35:41 +0800

    Update DKUIColorExt.swift

--- CocoaExtend/DKUIColorExt.swift
+++ CocoaExtend/DKUIColorExt.swift
@@ -20,14 +20,15 @@ extension UIColor {
      传入RGBA的16进制字符串，顺序必须是R,G,B,A，得到UIColor
      */
     convenience init(_ rgba:String) {
-        var r:CGFloat = 0.0, g:CGFloat = 0.0, b:CGFloat = 0.0, a:CGFloat = 1.0
+        var r:CGFloat = 0.0, g:CGFloat = 0.0, b:CGFloat = 0.0, a:CGFloat = 255.0
         if rgba.count >= 6 {
             if let rr = Int(rgba.substring(start: 0, lenth: 2), radix: 16) { r = CGFloat(rr) }
             if let gg = Int(rgba.substring(start: 2, lenth: 2), radix: 16) { g = CGFloat(gg) }
             if let bb = Int(rgba.substring(start: 4, lenth: 2), radix: 16) { b = CGFloat(bb) }
-        } else if rgba.count >= 8 {
-            if let aa = Int(rgba.prefix(2), radix: 16) { a = CGFloat(aa) }
         }
-        self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
+        if rgba.count >= 8 {
+            if let aa = Int(rgba.suffix(2), radix: 16) { a = CGFloat(aa) }
+        }
+        self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a / 255.0)
     }
 }
